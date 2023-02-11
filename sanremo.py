69c66234a3eb3409d0d510652584d210e69de1e4
baltornat <marcocesana98@gmail.com>
2023-02-11 15:31:42 +0100

    Imports

--- sanremo.py
+++ sanremo.py
@@ -1,7 +1,7 @@
 import dash
 import dash_table
 import dash_html_components as html
-import dash_core_components as dcc
+from dash import dcc
 import dash_bootstrap_components as dbc
 from dash.dependencies import Input, Output
 import pandas as pd
